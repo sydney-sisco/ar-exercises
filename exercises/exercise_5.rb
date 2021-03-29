3dfa710a39f6d495df43032cf706543fd2066554
Sydney Sisco <hello@sydsis.co>
2021-03-29 12:22:05 -0700

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "total revenue: #{Store.sum(:annual_revenue)}"
+puts "average revenue: #{Store.average(:annual_revenue)}"
+puts "number of stores with >$1M sales: #{Store.where(annual_revenue: 1000000..Float::INFINITY).size}"
