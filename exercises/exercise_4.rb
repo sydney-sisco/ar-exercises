4d34258aa1d13fce59b80dbda650188515ded2e9
Sydney Sisco <hello@sydsis.co>
2021-03-29 12:13:27 -0700

    complete exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,35 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+puts Store.all.count # 2
+
+store4 = Store.new
+store4.name = 'Surrey'
+store4.annual_revenue = 224000
+store4.womens_apparel = true
+store4.save
+
+store5 = Store.new
+store5.name = 'Whistler'
+store5.annual_revenue = 1900000
+store5.mens_apparel = true
+store5.save
+
+store6 = Store.new
+store6.name = 'Yaletown'
+store6.annual_revenue = 430000
+store6.mens_apparel = true
+store6.womens_apparel = true
+store6.save
+
+puts Store.all.count # 5
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
+
+@underperforming_womens_stores = Store.where(womens_apparel: true, annual_revenue: 0..1000000)
+@underperforming_womens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
