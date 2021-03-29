b7be69244d0eaa149cc4e6a78fae9feb4046a728
Sydney Sisco <hello@sydsis.co>
2021-03-29 11:55:24 -0700

    complete exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -11,17 +11,20 @@ end
 puts Store.all.count # 0
 
 store1 = Store.new
+store1.name = 'Burnaby'
 store1.annual_revenue = 300000
 store1.mens_apparel = true
 store1.womens_apparel = true
 store1.save
 
 store2 = Store.new
+store2.name = 'Richmond'
 store2.annual_revenue = 1260000
 store2.womens_apparel = true
 store2.save
 
 store3 = Store.new
+store3.name = 'Gastown'
 store3.annual_revenue = 190000
 store3.mens_apparel = true
 store3.save
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+
+@store2 = Store.find_by(id: 2)
+
+@store1.name = 'Burnaby South'
+@store1.save
